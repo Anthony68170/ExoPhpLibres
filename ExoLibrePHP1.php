fea4865bbbd3bd687d3f55e30f4e696c8e94d903
ANTHO_XB <160512730+Anthony68170@users.noreply.github.com>
2024-07-10 23:07:52 +0200

    first commit

---
+++ ExoLibrePHP1.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice libre PHP 1</title>
+    <style>
+        p {
+            font-weight : lighter;
+        }
+    </style>
+</head>
+<body>
+
+<h1>Exercice 1 : <br>
+    Afficher les éléments d'un tableau<h1>
+    <p>Crée un script PHP qui affiche chaque élément d'un tableau de fruits (Pomme, Banane, Orange et Fraise).<p>
+
+    <?php
+// ENREGISTRER LES FRUITS DANS UN TABLEAU (CONSULTABLE)
+$fruits = ["Pomme", "Banane", "Orange", "Fraise"];
+
+// FAIRE APPARAITRE LES ELEMENTS DU TABLEAU (LES FRUITS)
+$fruits = ["Pomme", "Banane", "Orange", "Fraise"];
+
+foreach ($fruits as $fruit) {
+    echo $fruit . "<br>";
+}
+
+    ?>
+</body>
+</html>
