b4c6d8295e939e58e37a0291f47045a9a60c94d2
ANTHO_XB <160512730+Anthony68170@users.noreply.github.com>
2024-07-11 00:22:24 +0200

    Modif ExoLibre PHP

---
+++ Exo2LibrePhp1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 2 libre PHP 1</title>
+
+    <style>
+        p {
+            font-weight : lighter;
+        }
+    </style>
+    
+</head>
+<body>
+
+<h1>Exercice 2 : Afficher les nombres pairs</h1>
+
+<p>Créer un script PHP qui affiche tous les nombres pairs entre 1 et 20.</p>
+    <?php
+    
+
+
+    ?>
+</body>
+</html>
