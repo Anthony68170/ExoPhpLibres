11246ff836c612abf0693b208f6dbe01c78960d6
ANTHO_XB <160512730+Anthony68170@users.noreply.github.com>
2024-07-11 09:20:34 +0200

    Exo3LibrePHP

---
+++ Exo3LibrePHP1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice PHP 1 Libre</title>
+
+    <style>
+
+        p{
+            font-weight : lighter;
+        }
+
+    </style>
+
+</head>
+<body>
+    <h1>Exercice 3 : Calculer la somme des éléments d'un tableau</h1>
+
+
+</body>
+</html>
