206473ec58f98a17b4486fd3859d8b37e4614952
ANTHO_XB <160512730+Anthony68170@users.noreply.github.com>
2024-07-11 10:02:28 +0200

    ModifStyleExo4Libre Php

---
+++ Exo4LibrePHP1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice Libre PHP</title>
+
+    <style> 
+        p{
+            font-weight : lighter;
+        }
+    </style>
+</head>
+<body>
+    
+    <h1>Exercice 4 : Afficher les éléments d'un tableau associatif</h1>
+    <p>Crée un script PHP qui affiche chaque clé et valeur d'un tableau associatif de pays et de leurs capitales. <br>
+    Exemple : <br>
+    ["France" => "Paris", "Allemagne" => "Berlin", "Espagne" => "Madrid"].</p>
+
+</body>
+</html>
