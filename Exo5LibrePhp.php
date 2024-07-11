c87a5a442aec8fd016d5d5bcd22a67cf2ed75f42
ANTHO_XB <160512730+Anthony68170@users.noreply.github.com>
2024-07-11 13:26:41 +0200

    Exo5LibrePhp

---
+++ Exo5LibrePhp.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 5 Libre PHP</title>
+
+    <style>
+        p{
+            font-weight :lighter;
+        }
+
+    </head>
+<body>
+<h1>Exercice 5 : Trouver le plus grand nombre dans un tableau</h1>
+<p>Crée un script PHP qui trouve et affiche le plus grand nombre dans un tableau de nombres [3, 5, 7, 2, 8, 1, 9].</p>
+
+</body>
+</html>
